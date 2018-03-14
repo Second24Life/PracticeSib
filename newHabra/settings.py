0d0372d60dc9640a5f68672971c13f0c53183674
Max Zerubitskiy <maka123456789@mail.ru>
2018-03-14 15:05:18 +0700

    correction

--- newHabra/settings.py
+++ newHabra/settings.py
@@ -12,11 +12,10 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import dj_database_url
-try:
-    import secret_keys
-except Exception as e:
-    pass
-
+# try:
+#     import secret_keys
+# except Exception as e:
+#     pass
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -28,13 +27,17 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'a51y8#5vshdaim58r2o(4gkf-eiup5vwtl(42j7=cd)b+7swqn'
-try:
-    SECRET_KEY = os.environ.get('SECRET_KEY', secret_keys.SECRET_KEY)
-    GOOGLE_RECAPTCHA_SECRET_KEY = os.environ.get('GOOGLE_RECAPTCHA_SECRET_KEY',
-                                             secret_keys.GOOGLE_RECAPTCHA_SECRET_KEY)
-except Exception as e:
-    pass
+# try:
+#     SECRET_KEY = os.environ.get('SECRET_KEY', secret_keys.SECRET_KEY)
+#     GOOGLE_RECAPTCHA_SECRET_KEY = os.environ.get('GOOGLE_RECAPTCHA_SECRET_KEY',
+#                                              secret_keys.GOOGLE_RECAPTCHA_SECRET_KEY)
+# except Exception as e:
+#     pass
 
+SECRET_KEY = os.environ.get(
+    'SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+GOOGLE_RECAPTCHA_SECRET_KEY = os.environ.get('GOOGLE_RECAPTCHA_SECRET_KEY',
+                                             '6Lfw60sUAAAAAN1DYj1cu68RzXDmkllnwCyIBaXe')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
